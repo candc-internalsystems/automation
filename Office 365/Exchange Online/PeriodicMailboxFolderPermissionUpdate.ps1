53fa969a9b81ce677a3c437ba89a89714593421d
Ashley Steel <ashley.steel@contentandcode.com>
2016-12-15 15:46:05 +1300

    Pre-run publish
    Added ability to only run the permission update against a specific group
    of users

--- Office 365/Exchange Online/PeriodicMailboxFolderPermissionUpdate.ps1
+++ Office 365/Exchange Online/PeriodicMailboxFolderPermissionUpdate.ps1
@@ -3,41 +3,62 @@
 #	- Don't change permissions if it is a level higher than intended setting (eg, owner?)
 
 Param(
-	$AzureAutomationCredential = "",
-	$GroupsToEnsure = @("Office","Contractors"),
+	$AzureAutomationCredential = "Exchange Online admin",
+	$GroupsToApplyTo = @("Office", "Contractors", "Rooms"),
+	$GroupsToAssignPermissionTo = @("Office","Contractors"),
 	$AccessRights = "Reviewer",
-	$AllowableAccessRights = @("Owner","Contributor"),
-	#$IdentityGroups = "Office", "Contractors", "Rooms", - not yet in use
+	$AllowableAccessRights = @("Owner","Publishing Editor","Editor","Publishing Author","Author","Nonediting Author"),
 	$Folder = "Calendar"
 );
 
+Write-Output "Getting credentials $AzureAutomationCredential..."
 $cred = Get-AutomationPSCredential -Name $AzureAutomationCredential;
 
+Write-Output "Targetted AccessRights: $AccessRights"
+$AllowableAccessRights += $AccessRights;
+Write-Output ("AllowableAccessRights: " + ($AllowableAccessRights -join ", ") );
+Write-Output "Targetted Folder: $Folder"
+
+Write-Output "Connecting to Exchange Online PSSession..."
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $cred -Authentication Basic -AllowRedirection;
 Import-PSSession $Session;
 
-$AllowableAccessRights+=$AccessRights;
-
 #$AllMailboxes = Get-Mailbox -ResultSize Unlimited;
-$AllMailboxes = Get-Mailbox -Identity "Mark.Grose"
+#$AllMailboxes = Get-Mailbox -ResultSize Unlimited -Filter {(MemberOfGroup -eq "")};
+#$AllMailboxes = Get-Mailbox -Identity "Mark.Grose"
 
-ForEach ( $group in $GroupsToHaveReviewer ) {
-	Foreach ($Mailbox in $AllMailboxes) {
+$AllGroupMembers = @();
+$AllMailboxesAliases = @();
+
+Write-Output ("GroupsToApplyTo they have the permissions added: " + ($GroupsToApplyTo -join ", "))
+$GroupsToApplyTo | % {
+	Write-Verbose "`t$_";
+	$AllGroupMembers += Get-DistributionGroupMember $_ -ResultSize Unlimited;
+	Write-Verbose ("`t`t" + (($AllGroupMembers | Select-Object -ExpandProperty Alias | Sort-Object) -join ", "));
+}
+
+Write-Output "Collecting all unique mailbox aliases..." 
+$AllMailboxesAliases = $AllGroupMembers | Select-Object -Unique alias -ExpandProperty alias | Sort-Object | ? { $_ -ne $null -and $_ -ne "" }
+
+Write-Output "Processing mailboxes now..."
+ForEach ( $group in $GroupsToAssignPermissionTo ) {
+	Foreach ($MailboxAlias in $AllMailboxesAliases) {
+		Write-Verbose "Getting existing $folder permission for $MailboxAlias..."
+
 		$ExistingPermission = $null;
-		$ExistingPermission = Get-MailboxFolderPermission -Identity ($Mailbox.Alias+':\'+$Folder) -User $Group -ErrorAction SilentlyContinue;
+		$ExistingPermission = Get-MailboxFolderPermission -Identity ($MailboxAlias+':\'+$Folder) -User $Group -ErrorAction SilentlyContinue;
 		
 		if ( $ExistingPermission -eq $null ) {
-			Write-Output ("Identity {0} does not have {1} access rights on their {3} for group '{2}'...adding..." -f $mailbox.alias, $AccessRights, $Folder);
-			Write-Output ("`tAdd-MailboxFolderPermission -Identity $($Mailbox.Alias+':\'+$Folder) -User $group -AccessRights $AccessRights;");
-			Add-MailboxFolderPermission -Identity ($Mailbox.Alias+':\'+$Folder) -User $group -AccessRights $AccessRights;
-		} elseif ( $ExistingPermission | ? { $AllowableAccessRights.Contains($_.AccessRights) } -eq $null ) {
-			#ToDo: Check .contains actually works here for this
-			#ToDo: Check if there are multiple ExistingPermissions (if possible?), and deal with accordingly
-			Write-Warning ("Identity {0} does not have {1} access rights on {4} folder for group '{2}', instead had {3}...reconfiguring..." -f $mailbox.alias, $AllowableAccessRights -join ", ", $group, $ExistingPermission.AccessRights, $Folder);
-			Write-Output ("`tSet-MailboxFolderPermission -Identity $($Mailbox.Alias+':\'+$Folder) -User $group -AccessRights $AccessRights;");
-			Set-MailboxFolderPermission -Identity ($Mailbox.Alias+':\'+$Folder) -User $group -AccessRights $AccessRights;
+			Write-Output ("Identity {0} does not have {1} access rights on their {3} for group '{2}'...adding..." -f $MailboxAlias, $AccessRights, $group, $Folder);
+			Write-Output ("Add-MailboxFolderPermission -Identity $($MailboxAlias+':\'+$Folder) -User $group -AccessRights $AccessRights;");
+			Add-MailboxFolderPermission -Identity ($MailboxAlias+':\'+$Folder) -User $group -AccessRights $AccessRights;
+		} elseif ( ($ExistingPermission.AccessRights | ? { $AllowableAccessRights.Contains($_) }) -eq $null ) {
+			#ToDo: Deal properly with multiple AccessRights, perhaps just use Add-?
+			Write-Warning ("Identity {0} does not have allowable access rights on {4} folder for group '{2}', instead had {3}...reconfiguring..." -f $MailboxAlias, $group, $ExistingPermission.AccessRights, $Folder);
+			Write-Output ("Set-MailboxFolderPermission -Identity $($MailboxAlias+':\'+$Folder) -User $group -AccessRights $AccessRights;");
+			Add-MailboxFolderPermission -Identity ($MailboxAlias+':\'+$Folder) -User $group -AccessRights $AccessRights;
 		} else {
-			Write-Verbose ("No change required for group {2} on identity {0}'s folder {1}" -f $Mailbox.Alias, $Folder, $group)
+			Write-Verbose ("No change required for group {2} on identity {0}'s folder {1} as its set as {3}" -f $MailboxAlias, $Folder, $group, $ExistingPermission.AccessRights);
 		}
 	}
 }
